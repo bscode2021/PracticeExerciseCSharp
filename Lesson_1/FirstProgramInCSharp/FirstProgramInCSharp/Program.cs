9c98fda0b4577ae5f0834c91398932a9288c5802
BScode <90622569+bscode2021@users.noreply.github.com>
2022-10-09 11:11:47 +0200

    Added exercise 1.01

---
+++ Lesson_1/FirstProgramInCSharp/FirstProgramInCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstProgramInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Boban!");
+        }
+    }
+}
