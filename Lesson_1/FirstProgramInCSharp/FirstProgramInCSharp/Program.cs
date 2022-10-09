ff5261064e804ff55999cd38c48147f950ad112a
BScode <90622569+bscode2021@users.noreply.github.com>
2022-10-09 11:14:23 +0200

    Added summary

--- Lesson_1/FirstProgramInCSharp/FirstProgramInCSharp/Program.cs
+++ Lesson_1/FirstProgramInCSharp/FirstProgramInCSharp/Program.cs
@@ -4,6 +4,10 @@ namespace FirstProgramInCSharp
 {
     internal class Program
     {
+        /// <summary>
+        /// Create a program (in C#) to print Hello on screen and then print your name (in aseparate line).
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
